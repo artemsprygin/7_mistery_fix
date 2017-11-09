84635cec43178dd714bc90c6ff2d7b1e11117ec3
artemsprygin <artemsprygin@me.com>
2017-11-09 12:11:16 +0300

    fix final

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,7 +1,13 @@
-import math
+from math import sqrt
 
 
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
-    return (-b - math.sqrt(discriminant)) / 2 if discriminant >= 0 else None,\
-           (-b + math.sqrt(discriminant)) / 2 if discriminant > 0 else None
+    if discriminant < 0:
+        return None, None
+    root1 = (-b - sqrt(discriminant)) / (2 * a)
+    root2 = (-b + sqrt(discriminant)) / (2 * a)
+    if discriminant == 0:
+        return root1, None
+    else:
+        return root1, root2
